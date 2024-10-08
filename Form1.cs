8ffa62aa984386137bff6155dbf9dff6813b2a8e
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2024-10-08 13:47:37 -0300

    criando as janelas de login e cadastro e prgramando as validações primarias

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,35 @@ namespace projetoAgenda
         {
             InitializeComponent();
         }
+        private void habilitarbotãodelogin()
+        {
+            if (textBox1.Text != "" && textBox2.Text.Length >= 8)
+            {
+                btn_logar.Enabled = true;
+
+            }
+            else
+                btn_logar.Enabled = false;
+        }
+            
+
+        private void cadastrar_Click(object sender, EventArgs e)
+        {
+            cadastro janela_cadastro = new cadastro();
+            janela_cadastro.ShowDialog();
+
+        }
+        // para abilitar o botao de login \\
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            habilitarbotãodelogin();
+        
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            habilitarbotãodelogin();
+        }
     }
-}
+    }
+
